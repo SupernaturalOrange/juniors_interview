4cb3c34548c4e98958f1bcf41365a03ab17ad20d
Miron-Anosov <mairon26rus@gmail.com>
2024-11-18 18:14:38 +0100

    style: up stype with black and flake8

--- task1/test_1.py
+++ task1/test_1.py
@@ -1,26 +1,28 @@
 """Test decorator strict"""
+
+import inspect
+import logging
+from functools import wraps
 from string import Template
 from typing import Callable
-from functools import wraps
-import logging
-import inspect
 
 logger = logging.getLogger(__name__)
 
 
 def strict(func: Callable) -> Callable:
-    """Декоратор проверяет соответствие типов переданных в вызов функции аргументов
-    типам аргументов, объявленным в прототипе функции.
-    """
+    """Декоратор проверяет соответствие типов."""
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         """Wrapper."""
         annotations = inspect.signature(func).parameters
-        error_msg = Template("Требуемый тип $arg_type, было передано $arg_type2, тип: $type")
+        error_msg = Template(
+            "Требуемый тип $arg_type, было передано $arg_type2, тип: $type"
+        )
         return_annotation = inspect.signature(func).return_annotation
 
         try:
-            for index,type_arg in enumerate(annotations.values()):
+            for index, type_arg in enumerate(annotations.values()):
                 if val := kwargs.get(type_arg.name):
                     if not isinstance(val, type_arg.annotation):
                         raise TypeError(
@@ -42,17 +44,18 @@ def strict(func: Callable) -> Callable:
 
             result = func(*args, **kwargs)
 
-            if return_annotation and  not isinstance(result, return_annotation):
-                raise TypeError(error_msg.substitute(
-                                arg_type=return_annotation.__name__,
-                                arg_type2=args[result],
-                                type=type(result).__name__,)
+            if return_annotation and not isinstance(result, return_annotation):
+                raise TypeError(
+                    error_msg.substitute(
+                        arg_type=return_annotation.__name__,
+                        arg_type2=args[result],
+                        type=type(result).__name__,
+                    )
                 )
 
         except TypeError as er:
             logger.error(er)
             raise TypeError(er)
-
 
     return wrapper
 
@@ -62,24 +65,26 @@ def my_func(params: dict, value: int, comment: str) -> bool:
     logger.info(f"params: {params}, value: {value}, comment: {comment}")
     return True
 
+
 try:
-    my_func(params={"a":1, "b":2, "c":3}, value=ValueError, comment="test")
+    my_func(params={"a": 1, "b": 2, "c": 3}, value=ValueError, comment="test")
 except TypeError as error:
     logger.error(error)
 
-my_func(params={"a":1, "b":2, "c":3}, value=22, comment="test")
+my_func(params={"a": 1, "b": 2, "c": 3}, value=22, comment="test")
 
-my_func({"a":1, "b":2, "c":3}, 22, "test")
+my_func({"a": 1, "b": 2, "c": 3}, 22, "test")
 
 try:
-    my_func({"a":1, "b":2, "c":3}, 22.22, comment="ValueError")
+    my_func({"a": 1, "b": 2, "c": 3}, 22.22, comment="ValueError")
 except TypeError as error:
     logger.error(error)
 
 try:
-    my_func({"a":1, "b":2, "c":3}, value=22.22, comment="ValueError")
+    my_func({"a": 1, "b": 2, "c": 3}, value=22.22, comment="ValueError")
 except TypeError as error:
     logger.error(error)
+
 
 @strict
 def sum_two(a: int, b: int) -> int:
@@ -91,4 +96,3 @@ try:
     print(sum_two(1, 2.4))  # >>> TypeError
 except TypeError as error:
     logger.error(error)
-
